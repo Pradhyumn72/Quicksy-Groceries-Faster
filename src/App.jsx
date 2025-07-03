bab1b83e302fab7028ac9626eafabd089a3fac64
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-07-03 17:17:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+
+
+const App = () => {
+  return (
+    <Layout/>
+  );
+};
+
+export default App;
---
+++ src/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Topmenu from './Components/TopMenu';
+const Layout = () => {
+  return (
+    <div>
+      <Header/>
+      <Topmenu/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default Layout;
